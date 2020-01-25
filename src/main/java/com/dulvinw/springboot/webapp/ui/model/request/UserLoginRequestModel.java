69924d4e37f2d46deb8188f42eca605c337693e6
dulvin <dulvinw@gmail.com>
2020-01-26 00:01:43 +0530

    Fix incorrect Copyright header

--- src/main/java/com/dulvinw/springboot/webapp/ui/model/request/UserLoginRequestModel.java
+++ src/main/java/com/dulvinw/springboot/webapp/ui/model/request/UserLoginRequestModel.java
@@ -1,19 +1,9 @@
 /*
- *  Copyright (c) 2019, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ * Copyright (c) 2020, FinWhaleLabs Inc. (http://www.finwhalelabs.com) All Rights Reserved.
  *
- *  WSO2 Inc. licenses this file to you under the Apache License,
- *  Version 2.0 (the "License"); you may not use this file except
- *  in compliance with the License.
- *  You may obtain a copy of the License at
+ * Author : Dulvin Witharane
+ * Date Created : 25/01/2020, 23:59
  *
- *    http://www.apache.org/licenses/LICENSE-2.0
- *
- *  Unless required by applicable law or agreed to in writing,
- *  software distributed under the License is distributed on an
- *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- *  KIND, either express or implied.  See the License for the
- *  specific language governing permissions and limitations
- *  under the License.
  */
 
 package com.dulvinw.springboot.webapp.ui.model.request;
